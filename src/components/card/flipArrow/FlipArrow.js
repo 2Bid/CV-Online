b3f8208f91d7611e8e6183d11953d42681453dbd
2Bid <salenbieraxel@gmail.com>
2023-04-03 20:01:49 +0200

    refractor

--- src/components/card/flipArrow/FlipArrow.js
+++ src/components/card/flipArrow/FlipArrow.js
@@ -3,17 +3,15 @@ import './flipArrow.css'
 
 export default function FlipArrow({reverse}) {
   return (
-    <div className="flipArrow__container">
-        <div className='flipArrow' onClick={reverse}>
-            <svg  className="flipArrow__svg" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" width="40px">            
+    <div className="flipArrow" onClick={reverse}>
+        <svg  className="flipArrow__svg" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" width="40px">            
             <path d="M20 12L4 12" stroke="#e3e5eb" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" >
                 <animate attributeName="stroke" values="#e3e5eb;#e3e5eb;#f0a818;#e3e5eb;#e3e5eb;#e3e5eb" dur="3s" repeatCount="indefinite" />
             </path>
             <path d="M14 18L19.9375 12.0625V12.0625C19.972 12.028 19.972 11.972 19.9375 11.9375V11.9375L14 6" stroke="#e3e5eb " strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                 <animate attributeName="stroke" values="#e3e5eb;#e3e5eb;#e3e5eb;#f0a818;#e3e5eb;#e3e5eb" dur="3s" repeatCount="indefinite" />
             </path>
-            </svg>
-        </div>
+        </svg>
     </div>
   )
 }
