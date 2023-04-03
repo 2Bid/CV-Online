a82f4950db0ae174d8cd2f3b1f344f2cbca60380
2Bid <salenbieraxel@gmail.com>
2023-04-03 18:40:42 +0200

    creation flipArrow components + added svg animation, add ref backside

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,26 +1,25 @@
 import { useState } from "react";
-import "./card.css";
-import "./flip-transition.css";
 import BackSide from "./side/back/BackSide";
 import FrontSide from "./side/front/FrontSide";
+import FlipArrow from "./flipArrow/FlipArrow";
+
+import "./card.css";
+import "./flip-transition.css";
 
 function Card({flip}) {
 
   const [isBackside, setIsBackside] = useState(false)
 
   function reverse(){
-    setIsBackside(!isBackside)
     flip()
+    setIsBackside(!isBackside)
   }
 
   return (
     <div className="card__container">
       <FrontSide/>
       <BackSide isBackside={isBackside}/>
-  
-      <span className='cta' onClick={reverse}>
-        <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" width="35px"><path d="M20 12L4 12" stroke="#e3e5eb" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path> <path d="M14 18L19.9375 12.0625V12.0625C19.972 12.028 19.972 11.972 19.9375 11.9375V11.9375L14 6" stroke="#e3e5eb " strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path></svg>  
-      </span>
+      <FlipArrow reverse={reverse}/>
     </div>
   );
 }
--- src/components/card/side/back/BackSide.js
+++ src/components/card/side/back/BackSide.js
@@ -1,20 +1,29 @@
-import React from 'react'
-import SkillCircle from './../../../skillCircle/SkillCircle'
+import React, { useEffect, useRef } from 'react'
+import {SkillCircle} from './../../../skillCircle/SkillCircle'
 import Carousel from '../../../Carousel/Carousel'
 
 import './backSide.css'
 
 export default function BackSide({isBackside}) {
+  
+  const backsideRef = useRef(false)
+
+  useEffect(()=>{
+    if(isBackside){
+      backsideRef.current = true
+    }
+  },[isBackside])
+
   return (
     <div className='cardSide__back'>
       <div className='hardskills__section'>
         <h2>HardSkills</h2>
         <div className="hardskills__container">
-          <SkillCircle name="HTML" percentage={90} isBackside={isBackside}/>
-          <SkillCircle name="CSS" percentage={80} isBackside={isBackside}/>
-          <SkillCircle name="JS" percentage={65} isBackside={isBackside}/>
-          <SkillCircle name="React" percentage={75} isBackside={isBackside}/>
-          <SkillCircle name="SEO" percentage={70} isBackside={isBackside}/>
+          <SkillCircle name="HTML" percentage={95} ref={backsideRef}/>
+          <SkillCircle name="CSS" percentage={80} ref={backsideRef}/>
+          <SkillCircle name="JS" percentage={65} ref={backsideRef}/>
+          <SkillCircle name="React" percentage={70} ref={backsideRef}/>
+          <SkillCircle name="SEO" percentage={60} ref={backsideRef}/>
         </div>
       </div>
       <div className='projects__section'>
