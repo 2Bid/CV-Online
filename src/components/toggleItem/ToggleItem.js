0bd2e48cb8f33df53ad16bbfef72156fc9d2d43c
2Bid <salenbieraxel@gmail.com>
2023-04-03 18:13:24 +0200

    update component toggleItem

--- src/components/toggleItem/ToggleItem.js
+++ src/components/toggleItem/ToggleItem.js
@@ -4,30 +4,32 @@ import './toggleItem.css'
 export default function ToggleItem({title, date,reward, list}) {
 
     const toggling = (e) =>{
-        const allToggle = document.querySelectorAll('.toggle__content')
+        const allToggle = document.querySelectorAll('.toggle__item')
 
         allToggle.forEach(toggle => {
-            toggle.style.height = 0
+            toggle.querySelector('.toggle__content').style.height = 0
             toggle.classList.remove('active')
         });
 
         e.target.lastChild.style.height = `${e.target.lastChild.scrollHeight}px`
-        e.target.lastChild.classList.add('active')
+        e.target.classList.add('active')
     }
 
   return (
-        <div className='toggle__item' onClick={(e)=>toggling(e)}>
-            <h4 className='toggle__title'>{title}</h4>
-            <div className='toggle__content'>
-                <span className='toggle__date'>{date}</span>
-                <span className='toggle__date'>{reward}</span>
-                <ul className='toggle__list'>
-                    {
-                        list.map((item, id)=>{
-                            return <li className='toggle__list-item' key={id}>{item}</li>
-                        })
-                    }
-                </ul>
+        <div className='toggle__item__container' onClick={(e)=>toggling(e)}>
+            <div className='toggle__item'>
+                <h4 className='toggle__title'>{title}</h4>
+                <div className='toggle__content'>
+                    <span className='toggle__date'>{date}</span>
+                    <span className='toggle__date'>{reward}</span>
+                    <ul className='toggle__list'>
+                        {
+                            list.map((item, id)=>{
+                                return <li className='toggle__list-item' key={id}>{item}</li>
+                            })
+                        }
+                    </ul>
+                </div>
             </div>
         </div>
   )
