c44e88272794d42d887a47bfe0f77d372eaa9feb
2Bid <salenbieraxel@gmail.com>
2023-04-14 02:04:46 +0200

    refactor

--- src/components/toggleItem/ToggleItem.js
+++ src/components/toggleItem/ToggleItem.js
@@ -1,25 +1,40 @@
-import React from 'react'
+import React, { useLayoutEffect, useRef } from 'react'
 import './toggleItem.css'
 
 export default function ToggleItem({active, title, date,reward, list}) {
 
+    const toggleContent = useRef(null)
+
+    useLayoutEffect(()=>{
+        if(active){
+            const firstToggle = toggleContent.current
+            firstToggle.style.height = `${firstToggle.scrollHeight}px`
+        }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[])
+
     const toggling = (e) =>{
         const allToggle = document.querySelectorAll('.toggle__item')
 
-        allToggle.forEach(toggle => {
-            toggle.querySelector('.toggle__content').style.height = 0
-            toggle.classList.remove('active')
-        });
+        if(e.target.className === "toggle__content-container"){
+            return
+        }
+        else{
+            allToggle.forEach(toggle => {
+                toggle.querySelector('.toggle__content').style.height = 0
+                toggle.classList.remove('active')
+            });
 
-        e.target.lastChild.lastChild.style.height = `${e.target.lastChild.lastChild.scrollHeight}px`
-        e.target.classList.add('active')
+            e.target.querySelector('.toggle__content').style.height = `${e.target.querySelector('.toggle__content').scrollHeight}px`
+            e.target.classList.add('active')
+        }
     }
 
   return (
     <div className={`toggle__item ${active ? 'active' : ''}`} onClick={(e)=>toggling(e)}>
         <h4 className='toggle__title'>{title}</h4>
         <div className='toggle__content-container'>
-            <div className='toggle__content'>
+            <div className='toggle__content' ref={toggleContent}>
                 <span className='toggle__date'>{date}</span>
                 <span className='toggle__date'>{reward}</span>
                 <ul className='toggle__list'>
