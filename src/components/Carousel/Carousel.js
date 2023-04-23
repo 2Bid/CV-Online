c39f1db06a850cf66769862d6f404c7d0e5aae5f
2Bid <salenbieraxel@gmail.com>
2023-04-23 15:08:26 +0200

    update link github

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -47,16 +47,16 @@ export default function Carousel() {
 
   const projects = [
     /* OpenClassrooms */
-    <Project name="Reservia" link="https://2bid.github.io/AxelSalenbier_2_07032021/" img={Reservia} description={<p className='project__description'>- Intégration HTML CSS</p>}/>,
-    <Project name="Oh my food" link="https://2bid.github.io/AxelSalenbier_3_30032021/" img={Ohmyfood} description={<p className='project__description'>- Intégration HTML<br/>- Animation CSS</p>}/>,
+    <Project name="Reservia" link="https://2bid.github.io/Reservia/" img={Reservia} description={<p className='project__description'>- Intégration HTML CSS</p>}/>,
+    <Project name="Oh my food" link="https://2bid.github.io/OhMyFood/" img={Ohmyfood} description={<p className='project__description'>- Intégration HTML<br/>- Animation CSS</p>}/>,
     <Project name="Gameon" link="https://2bid.github.io/GameOn-website-FR/starterOnly/" img={Gameon} description={<p className='project__description'>- Reprise d'un repo forké<br/>- Ajout Fonction Javascript<br/>- Test manuels</p>}/>,
-    <Project name="FishEye" link="https://2bid.github.io/AxelSalenbier_6_010721/" img={fisheye} description={<p className='project__description'>- Intégration<br/>- Création d'un design pattern<br/>- Mise en avant Accessibilité</p>}/>,
-    <Project name="Les petits plats" link="https://2bid.github.io/AxelSalenbier_7_06102021/" img={plats} description={<p className='project__description'>- Analyse d'un problème<br/>- Développement d'un algorithme de recherche JS</p>}/>,
-    <Project name="Billed App" link="https://github.com/2Bid/AxelSalenbier_9_01022022" img={Billedapp} description={<p className='project__description'>- Ecriture de test unitaire<br/>- Plan de test end-to-end<br/>- Test d'intégration</p>}/>,
-    <Project name="Kasa" link="https://2bid.github.io/AxelSalenbier_11_01042022/" img={Kasa} description={<p className='project__description'>- Intégration Application React</p>}/>,
-    <Project name="SportSee" link="https://2bid.github.io/AxelSalenbier_12_24042022/" img={SportSee} description={<p className='project__description'>- Intégration Application React<br/>- Intéraction API<br/>- Documentation Js</p>}/>,
-    <Project name="BankAPI" link="https://github.com/2Bid/AxelSalenbier_13_27072022" img={argentbank} description={<p className='project__description'>- Mise en place Redux toolkit<br/>- Intéraction et Authentification API<br/>- Création Endpoints</p>}/>,
-    <Project name="Datepicker input" link="https://2bid.github.io/AxelSalenbier_14_01112022/" img={datepicker} description={<p className='project__description'>- Création d'une librarie react<br/>- Conversion de Jquery à React<br/>- Analyse de performances</p>}/>,
+    <Project name="FishEye" link="https://2bid.github.io/FishEye/" img={fisheye} description={<p className='project__description'>- Intégration<br/>- Création d'un design pattern<br/>- Mise en avant Accessibilité</p>}/>,
+    <Project name="Les petits plats" link="https://2bid.github.io/LesPetitsPlats/" img={plats} description={<p className='project__description'>- Analyse d'un problème<br/>- Développement d'un algorithme de recherche JS</p>}/>,
+    <Project name="Billed App" link="https://github.com/2Bid/BilledApp" img={Billedapp} description={<p className='project__description'>- Ecriture de test unitaire<br/>- Plan de test end-to-end<br/>- Test d'intégration</p>}/>,
+    <Project name="Kasa" link="https://2bid.github.io/Kasa/" img={Kasa} description={<p className='project__description'>- Intégration Application React</p>}/>,
+    <Project name="SportSee" link="https://2bid.github.io/SportSee/" img={SportSee} description={<p className='project__description'>- Intégration Application React<br/>- Intéraction API<br/>- Documentation Js</p>}/>,
+    <Project name="Argent Bank" link="https://github.com/2Bid/ArgentBank" img={argentbank} description={<p className='project__description'>- Mise en place Redux toolkit<br/>- Intéraction et Authentification API<br/>- Création Endpoints</p>}/>,
+    <Project name="HRnet" link="https://2bid.github.io/HRnet" img={datepicker} description={<p className='project__description'>- Création d'une librarie react<br/>- Conversion de Jquery à React<br/>- Analyse de performances</p>}/>,
     /* Projet Pers */ 
     <Project name="GamePreview" link="https://github.com/2Bid/game-preview-v2" img={gamepreview} description={<p className='project__description'>- Application React<br/>- Connexion API Rawg.io</p>}/>
   ]
